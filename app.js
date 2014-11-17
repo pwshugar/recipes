e9f1445496bc1f27836abffe0d98069181f42721
Peter Shugar <pwshugar@gmail.com>
2014-11-17 13:48:21 -0800

    port

--- app.js
+++ app.js
@@ -9,4 +9,7 @@ app.get('/', function(req, res){
   res.render('index.html');
 });
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Express server listening on port " + port);
+});
