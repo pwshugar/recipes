9cfe8e6b7261dde89d4901728e3fff73cb06969b
Peter Shugar <pwshugar@gmail.com>
2014-11-20 01:01:30 -0800

    added color coding and clickable option

--- public/list.js
+++ public/list.js
@@ -2,6 +2,28 @@ angular.module('RecipeApp')
 	.controller('ListCtrl', function ($scope, $rootScope, GroceryService){
 		$rootScope.addNewButton = false;
 		$scope.groceries = GroceryService.get();
+
+		$scope.vegetable = [];
+		$scope.fruit = [];
+		$scope.other = [];
+		sortGroceries();
+
+		
+	  function sortGroceries(){
+			for (var k in $scope.groceries){
+				$scope[$scope.groceries[k].type].push(k);
+			}
+			sortByQty($scope.vegetable);
+			sortByQty($scope.fruit);
+			sortByQty($scope.other);
+		};
+
+		function sortByQty(arr){
+			return arr.sort(function(a, b){
+				return $scope.groceries[a].qty - $scope.groceries[b].qty
+			});
+		};
+
 	});
 
 
